c166a1c3a18ba299a4d866c190ea3edf763a65b8
matbios <mathieubarascou@gmail.com>
2014-07-08 13:10:18 +0200

    exercice age en jours

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -2,7 +2,11 @@
 # Ruby library, giving you access to the Date class.
 require 'date'
 
-
 def age_in_days(day, month, year)
+date_reformatee = Date.new(year,month,day)
+delta = Date.today - date_reformatee
+return delta.to_i
+
   #TODO: return the age expressed in days given the day, month, and year of birth
+  # age_in_days(1, 1, 2014)
 end
