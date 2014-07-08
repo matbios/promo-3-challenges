4e8b276a0198900a9bef913e7c66855e81db0df4
matbios <mathieubarascou@gmail.com>
2014-07-08 15:33:24 +0200

    stupid coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,21 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+
+  if your_message == "I am going to work right now SIR !"
+  return ""
+
+  elsif your_message.include? "?"
+  return "Silly question, get dressed and go to work !"
+
+  else
+  return "I don't care son, get dressed and go to work !"
+  end
+
 end
+# TODO: return coach answer to your_message
 
 def coach_answer_enhanced(your_message)
+ if your_message == "You're a good boy"
+    return "yes i know"
+  end
   # TODO: return coach answer to your_message, with additional custom rules of yours !
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,8 @@
 require_relative "coach_answer"
+
+puts "Que souhaitez-vous dire a votre coach"
+ask = gets.chomp
+puts coach_answer(ask)
+
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
