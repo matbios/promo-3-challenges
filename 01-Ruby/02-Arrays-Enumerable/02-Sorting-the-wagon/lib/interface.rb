d77e27d1a7bdd0bd9c9f8de9442bd85d520b7490
matbios <mathieubarascou@gmail.com>
2014-07-09 14:21:27 +0200

    wagon sort answer

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -7,8 +7,26 @@ begin
   name = gets
   name.chomp! if name
 
+  students << name if name != ""
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+
+count = students.size
+list = wagon_sort(students)
+
+puts "Congratulations! Your Wagon has #{count} students:"
+#puts list
+
+puts wagon_displayname(list, count)
+
+
+
+# voilà :
+#puts "Congratulations! Your Wagon has #{count} students:"
+#print wagon_sort(students)
+#sleep(1)
+#wagon_sorted = wagon_sort(students)
+#puts wagon_wrongdisplay(wagon_sorted)
 
 # TODO: Call `wagon_sort` method and display the sorted student list
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,17 @@
 def wagon_sort(students)
+  return students.sort_by{|word| word.downcase}
   # TODO: return a copy of students, sorted alphabetically
 end
+
+def wagon_displayname(students, count)
+
+partie1 = students[0..count-2].join(", ")
+partie2 = students.last
+latotale = "#{partie1} and #{partie2}"
+return latotale
+
+end
+
+# afficher tous les éléments du tableau sur une même ligne
+# afficher des virgules entre les éléments du tableau sur la même ligne
+# afficher un "end" avant le dernier élément à la place de la virgule.
