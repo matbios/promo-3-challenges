faa597a51ee4e7a1c20f2ffd286c93664e35eabe
matbios <mathieubarascou@gmail.com>
2014-07-10 14:07:43 +0200

    fin exo

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -13,10 +13,21 @@ end
   #TODO: Build HTML tags around content given in the block
   # The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
 
+
+
 def timer_for
-  #TODO:  Return time taken to execute the given block
+  start = Time.now
+  yield
+  duree = Time.now - start
 end
+#TODO:  Return time taken to execute the given block
+# aller chercher le temps au début, demander le temps à la fin, et afficher la différence.
 
 def transform(element)
+  yield (element)
   #TODO:  Simply execute the given block on element
 end
+
+
+#puts timer_for
+#puts tag("h1", ["id", "title"]) {"mon block"}
