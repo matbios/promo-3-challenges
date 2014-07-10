e66112e532df090e371ec4651859b0c32502edeb
matbios <mathieubarascou@gmail.com>
2014-07-10 11:54:12 +0200

    derniere version

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,26 @@
-def pmu_format!(race_array)
+def pmu_format!(*race_array)
   #TODO: modify the given array so that it is PMU-consistent
+
+  for horse in race_array
+
+    rank = race_array.find_index(horse)
+
+    race_array[rank] = "#{rank + 1}-#{horse}"
+
+  end
+
+  race_array.reverse!
+
 end
 
+p pmu_format!("Abricot du Laudot", "Black Caviar", "Brigadier Gerard")
+
+# récupérer les données du tableau que j'ai reçu
+# parser les données que j'ai reçu
+# les inverser
+# rajouter un le rang devant
 
 
 
+#Ex: `["Abricot du Laudot", "Black Caviar", "Brigadier Gerard"]`
+#should become exaclty `["3-Brigadier Gerard!", "2-Black Caviar!", "1-Abricot du Laudot!"]`
