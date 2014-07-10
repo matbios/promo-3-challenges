0f8b464470c83119eb2db581c3ea7b71103327e9
matbios <mathieubarascou@gmail.com>
2014-07-10 15:17:19 +0200

    push de l exo sur les primes numbers

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,18 +1,31 @@
 def shuffle_word(a_word)
-  #TODO: refactor this method
-  
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-  
-  shuffle_array
-  
+  a_word.upcase.chars.shuffle
 end
+
+  #TODO: refactor this method
+  #cap_word = a_word.upcase
+  #chars_enum = cap_word.chars
+  #array = chars_enum.to_a
+  #shuffle_array = array.shuffle
+
+  #shuffle_array
 
 
 def quote_prime_numbers(n)
-  #TODO: refactor this method
-  
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  prime_numbers = (1..n).find_all do |i|
+    prime_test = (2..i-1).select {|k| i % k == 0 }
+    prime_test.count == 0
+  end
+  prime_numbers.map{ |prime_num| "#{prime_num} is prime"}
 end
+
+# (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+#TODO: refactor this method
+# avec "p" il retourne toutes les valeurs qu'il trouve, s'il n'y est pas il retourne la dernière
+
+# require 'prime'
+# def quote_prime_numbers(n)
+#   Prime.each(n) do |prime|
+#     print "#{prime} is prime"
+#   end
+# end
