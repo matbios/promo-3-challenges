aad080a9b2c6041617fd3f7322889bfe8fb6db56
matbios <mathieubarascou@gmail.com>
2014-07-11 14:09:52 +0200

    symbols resolution

--- 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
@@ -8,30 +8,40 @@
 # irb> "foo".object_id
 
 def are_identical_symbols_same_objects?
+  true
   # TODO: true or false?
 end
 
 def are_identical_strings_same_objects?
+  false
   # TODO: true or false?
 end
-
 
 # Remember, RTFM! Your doc is your friend
 # - http://www.ruby-doc.org/core-2.1.1/String.html
 # - http://www.ruby-doc.org/core-2.1.1/Symbol.html
 
 def convert_string_to_symbol(a_string)
+  a_string.to_sym
   # TODO: return the symbol version of "a_string"
+  #use to_sym
 end
 
 def convert_symbol_to_string(a_symbol)
+  a_symbol.to_s
   # TODO: return the string version of "a_symbol"
+  #use to_s
 end
 
 def me
+  me = {
+    age: 32,
+    name: "Mat"
+  }
   # TODO: return a Hash representing yourself, with keys such as age and name
 end
 
 def fruits
+  fruits = ["abricot", "apple"]
   # TODO: return an array of fruits
 end
