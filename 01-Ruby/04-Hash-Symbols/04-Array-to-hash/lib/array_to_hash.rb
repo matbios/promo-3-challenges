8255a2243107bcf89f80a9d377895008fadcd3bc
matbios <mathieubarascou@gmail.com>
2014-07-11 15:50:25 +0200

    array to hash resolution

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,28 @@
 def array_to_hash(array)
-  #TODO: implement the method :)
+  hash = Hash.new
+    array.each_with_index do |element, index|
+      if block_given?
+      hash[yield(index)] = element
+      else
+      hash[index] = element
+      end
+    end
+  return hash
 end
+
+puts array_to_hash([ "TITI", "TOTO" ])
+
+
+  #TODO: implement the method :)
+# transformer un array en hash
+# array_to_hash(["blabla", "nunu"])
+
+
+# array_to_hash([ "TITI", "TOTO" ])
+# { 0 => "TITI", 1 => "TOTO" }
+
+
+# array_to_hash([ "TITI", "TOTO" ]) do |index|
+#   "La cle #{index}"
+# end
+# => { "La cle 0" => "TITI", "La cle 1" => "TOTO" }
