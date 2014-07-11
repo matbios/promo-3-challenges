7c8482fc4098fa36dbe3fe6bd50684837a4ed021
matbios <mathieubarascou@gmail.com>
2014-07-11 12:20:17 +0200

    mc do resolution

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,47 @@
 def poor_calories_counter(burger, side, beverage)
-  #TODO: return number of calories for this mcDonald order
+
+calories = {
+"Cheese Burger" => 290,
+"Big Mac" => 300,
+"Mc Bacon" => 400,
+"Royal Cheese" => 130,
+"French fries" => 130,
+"Potatoes" => 130,
+"Coca" => 160,
+"Sprite" => 170
+}
+
+total_calories = calories[burger] + calories[side] + calories[beverage]
+#puts total_calories
 end
 
+#TODO: return number of calories for this mcDonald order
+# poor_calories_counter("Big Mac", "Potatoes", "Sprite")
+
+
 def calories_counter(*orders)
-  #TODO: return number of calories for a less constrained order
+
+  calories = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170,
+    "Happy Meal" => poor_calories_counter("Cheese Burger", "French fries", "Coca"),
+    "Best of Big Mac" => poor_calories_counter("Big Mac", "French fries", "Coca"),
+    "Best Of Royal Cheese" => poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
+  }
+
+  total_calories = 0
+  orders.each do |one_order|
+    total_calories += calories[one_order]
+  end
+  return total_calories
+    #TODO: return number of calories for a less constrained order
 end
+
+puts calories_counter("Happy Meal", "Mc Bacon")
+
