db92105301be5ec0329bd68abfa7a0a69093c8ce
matbios <mathieubarascou@gmail.com>
2014-07-15 11:54:56 +0200

    check du numero de telephone : done

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,23 @@
 def french_phone_number?(phone_number)
-  # TODO: true or false?
+  if phone_number =~ /^0[1-9][0-9]{8}$|^0[1-9]( [0-9][0-9]){4}$|^0[1-9](-[0-9][0-9]){4}$|^\+33[1-9][0-9]{8}$|^\+33 [1-9]( [0-9][0-9]){4}$/
+    true
+  else
+    false
+  end
 end
+
+# TODO: true or false?
+
+# It is valid when starting with a 0 and containing 10 digits
+# It is valid when starging with +33 and containing 11 digits
+
+
+# /^0|+33/  # starts with abc
+
+
+
+# ^0[1-9][0-9]{8}$|
+# ^0[1-9]( [0-9][0-9]){4}$|
+# ^0[1-9](-[0-9][0-9]){4}$|
+# ^\+33[1-9][0-9]{8}$|
+# ^\+33 [1-9]( [0-9][0-9]){4}$
