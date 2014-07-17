0fa06ded6aa8f0852b612c05f9fe7e4a777f4126
matbios <mathieubarascou@gmail.com>
2014-07-17 14:45:45 +0200

    tierce et animal - resolution

--- 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
@@ -1,26 +1,23 @@
 module DemoModule
-
-  class DemoClass  
-    
+  class DemoClass
     def self.introduce
+      2
       #TODO: choose return between 1, 2, 3
     end
-
     def introduce
+      3
       #TODO: choose return between 1, 2, 3
     end
-
   end
-  
   def self.introduce
+    1
     #TODO: choose return between 1, 2, 3
   end
-
 end
 
 def has_tierce?
   first = DemoModule::introduce
-  second = DemoModule::DemoClass.introduce      
+  second = DemoModule::DemoClass.introduce
   third = DemoModule::DemoClass.new.introduce
   [first, second, third] == [1, 2, 3]
 end
