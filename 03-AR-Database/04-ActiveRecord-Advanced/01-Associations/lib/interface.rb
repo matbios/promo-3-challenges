350866a4a75f8bc3884fe2d27fe7d66d3b906323
matbios <mathieubarascou@gmail.com>
2014-07-29 15:28:24 +0200

    essage=association resolution

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
@@ -2,6 +2,16 @@ require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
 
+User.destroy_all
+
+User.create(name: "bill", email: "bill@bill.com")
+User.create(name: "bob", email: "bob@bill.com")
+User.create(name: "billy", email: "billy@bill.com")
+User.create(name: "brando", email: "brando@bill.com")
+
+#p User.all
+
+
 def ask(prompt)
   print "#{prompt} "
   gets.to_s.chomp
@@ -12,25 +22,35 @@ def create_post(user)
   source_url = ask('Source URL:')
   rating = ask('Rating:')
   post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
-
+  new_post = Post.create(post_params)
+  user.posts << new_post
   # TODO: use ActiveRecord to add a new post for the user logged in!
 end
 
 def list_posts(user)
+  p user.posts
   # TODO: use ActiveRecord to get all posts of the current user
 end
 
 def delete_posts(user)
+  user_posts = user.posts
+  user_posts.destroy_all
   # TODO: use ActiveRecord to delete all posts of current user
 end
 
-while true
+
+
   logged_in = false
 
   until logged_in
     puts 'Please login with your <id>'
-    # TODO: instantiate a user with his <id>
+    id = gets.chomp.to_i
+    logged_in = true
+    user = User.find(id)
+
   end
+
+  while true
 
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
@@ -38,7 +58,7 @@ while true
   puts "3. Delete all posts"
   puts "4. Exit"
 
-  case ask('>')
+  case ask('>').to_i
   when 1 then create_post(user)
   when 2 then list_posts(user)
   when 3 then delete_posts(user)
