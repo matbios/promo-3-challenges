3a223c1120eeeace582f06d4eead3f0196a5b701
matbios <mathieubarascou@gmail.com>
2014-08-04 16:27:34 +0200

    =message comportement ok

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,13 @@
   * TODO: add your code here!
   *
 */
+
+
+$(document).ready(function(){
+    $('#message-wrapper').delay(500).slideDown(1000);
+});
+
+$( "#message-wrapper" ).click(function() {
+  $( "#message-wrapper" ).slideUp( "slow", function() {
+  });
+});
