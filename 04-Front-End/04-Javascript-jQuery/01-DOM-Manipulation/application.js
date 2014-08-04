86b452b3cda25c0fce32393eb631085fae5abca8
matbios <mathieubarascou@gmail.com>
2014-08-04 16:02:28 +0200

    =push the code

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,46 +8,51 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
-
+  // TODO: replace null, keep the email variable.
+  var email = $('#email').attr('value');
 
   // Ex 2. Fill the content of the email input with your email
-
-
+  $('#email').val('toto@tutu.com');
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
+  $('#email-hint').html('<strong>This is my email now</strong>');
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
-
+  $('th').addClass( 'blue' );
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
-
+  var team_count = $('tbody tr').length;  // TODO: replace 0, keep the team_count variable.
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+  $('table').append('<tr><td>15</td><td>la super team</td><td>0</td></tr>');
 
 
   // Ex 7. Write some code to sum all points given to all teams
-  var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
+  // TODO: replace 0 with your sum result, keep the sum variable.
 
+  var sum = 0;
+  $('tbody tr').each(function(i, value) {
+      sum += parseInt($(value).find('td:last').text());
+  });
+  // sur toutes les fonction each on doit ajouter i et value, c'est comme ça.
+  // le .text() permet de prendre le texte sans le HTML autout de la last td
+  // et le parseInt permet de transformer le texte en integer, pouyr pouvoir additionner les valeurs.
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
+  $('thead tr th').css("background-color","#DDF4FF");
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-
+  $('#results-wrapper').css("padding-top","20px");
 
 
   // Ex 10. Remove the "Email:" label from the DOM
-
+  $( "label" ).remove();
 
 
   // Checking exercise answers. DO NOT MODIFY BELOW
