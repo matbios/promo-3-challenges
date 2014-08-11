65f81dad894389d0ac06dfca8b571ca20d1a6388
matbios <mathieubarascou@gmail.com>
2014-08-11 15:19:20 +0200

    =correction

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -2,78 +2,41 @@ require 'open-uri'
 require 'json'
 
 def generate_grid(grid_size)
-  #TODO: generate random grid of letters
-  letters = ("A".."Z").to_a
-  letters_rand = []
+  Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+end
 
-  for n in 1..grid_size
-    letters_rand << letters.sample(1)
-  end
 
-  return letters_rand
+def is_included?(guess, grid)
+  guess.split("").all?{ |letter| grid.include? letter }
+end
+
+def compute_score(attempt, time_taken)
+  (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
 end
 
 def run_game(attempt, grid, start_time, end_time)
-  #TODO: runs the game and return detailed hash of result
+  result = { time: end_time - start_time, score: 0 }
 
-  time = end_time - start_time
+  result[:translation] = get_translation(attempt)
 
-  result = {}
-
-  if check(attempt, grid)
-    if translation(attempt).nil?
-      result[:translation] = nil
-      result[:score] = 0
-      result[:time] = time
-      result[:message] = "not an english word"
-    else
-      result[:translation] = translation(attempt)
-      result[:score] = score(attempt, time)
-      result[:time] = time
+  unless result[:translation]
+    result[:message] = "not an english word"
+  else
+    if is_included?(attempt.upcase, grid)
+      result[:score] = compute_score(attempt, result[:time])
       result[:message] = "well done"
+    else
+      result[:message] = "not in the grid"
     end
-  else
-    result[:translation] = translation(attempt)
-    result[:score] = 0
-    result[:time] = time
-    result[:message] = "not in the grid"
   end
 
-  return result
+  result
 end
 
 
-
-def check(attempt, grid)
-  word_split = attempt.upcase
-  word_split = word_split.split('')
-  word_split.all? do |x|
-    grid.include?(x)
-  end
+def get_translation(word)
+  response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.downcase}")
+  json = JSON.parse(response.read.to_s)
+  json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
 end
 
-
-def translation(attempt)
-
-  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/#{attempt}"
-
-  api_return = Hash.new
-  open(api_url) do |stream|
-    api_return = JSON.parse(stream.read)
-  end
-
-  if api_return.has_key? "Error"
-    return nil
-  else
-    return api_return["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
-  end
-
-end
-
-
-def score(attempt, time)
-
-  scoring = attempt.size + (1.to_f / time)
-  return scoring
-
-end
