993ecc11e6c6a5587c24dc99a7344471c271c856
matbios <mathieubarascou@gmail.com>
2014-08-11 11:59:38 +0200

    =pour un git propre

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
@@ -8,6 +8,32 @@ require 'open-uri'
 
 puts 'Seeding database...'
 
+
+10.times do
+  User.create(
+  name:                   Faker::Name.name,
+  email:                  Faker::Internet.email,
+)
+end
+
+
 # TODO: Your code goes here
 # 1. create fake users
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+
+def save_posts
+  doc = Nokogiri::HTML(open("https://news.ycombinator.com/"))
+
+  doc.search('.title a').each do |element|
+    name = element.content[/">([^<]*)<[^<]a[^<]/]
+    source_url = element.content[/href="(.*)\/">/]
+    rating = element.content[/>([^<]*)points/]
+    date = element.content[/\/a>([^<]*)\|/]
+
+    post_params = { name: name, source_url: source_url, rating: rating, cdate: date}
+    new_post = Post.create(post_params)
+    user.posts << new_post
+
+  end
+
+end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/interface.rb
@@ -2,4 +2,6 @@ require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
 
+p User.all
+p Post.all
 # TODO: build your interface with login and CRUD tasks on the posts
