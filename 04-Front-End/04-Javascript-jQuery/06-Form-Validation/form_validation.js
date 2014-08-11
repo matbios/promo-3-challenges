a69ba842841b7527a5c69424caa7143e6e846b0f
matbios <mathieubarascou@gmail.com>
2014-08-11 12:01:35 +0200

    =pour un git propre

--- 04-Front-End/04-Javascript-jQuery/06-Form-Validation/form_validation.js
+++ 04-Front-End/04-Javascript-jQuery/06-Form-Validation/form_validation.js
@@ -1,3 +1,14 @@
 // Your validation code will go in there.
 // Write your validation functions, and bind events
 // in a jQuery DOM ready callback
+
+// Make sure to check that all fields are filled.
+// Make sure the Terms of Service checkbox is ticked
+// Validate the zipcode format. We want to check for a French one only.
+// Validate the email format.
+// Validate the phone number. We want to accept French mobile phones only.
+// If all is in order, activate the submit button so that the user can post the form.
+
+
+$(document).ready(function() {
+});
